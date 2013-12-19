15550591f07c940db98dc4cddc501fabb7b3fe65
Ben <me@benng.me>
2013-12-18 20:04:25 -0600

    Send the directory of the entry file into rework-npm as cwd

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var importcss = require('rework-npm')
   , rework = require('rework')
   , variables = require('rework-vars')
+  , path = require('path')
   , read = require('fs').readFileSync
 
 module.exports = function (opts, cb) {
   opts = opts || {}
   var css = rework(read(opts.entry, 'utf8'))
-  css.use(importcss())
+  css.use(importcss(path.dirname(opts.entry)))
   if (opts.variables) {
     css.use(variables(opts.variables))
   }
