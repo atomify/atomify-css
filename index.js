1a54820bbac1c3fff9f68ecee7b979cb9679bbc8
Ben Clinkinbeard <ben.clinkinbeard@gmail.com>
2014-01-24 08:53:46 -0500

    Add opts.plugins for specifying additional rework plugins

--- index.js
+++ index.js
@@ -8,9 +8,18 @@ module.exports = function (opts, cb) {
   opts = opts || {}
   var css = rework(read(opts.entry, 'utf8'))
   css.use(importcss(path.dirname(opts.entry)))
+  
   if (opts.variables) {
     css.use(variables(opts.variables))
   }
+
+  // utilize any custom rework plugins provided
+  if (opts.plugins) {
+    opts.plugins.forEach(function (plugin) {
+      css.use(plugin)
+    })
+  }
+
   if (opts.transform) {
     opts.transform(css.toString(), cb)
   } else {
